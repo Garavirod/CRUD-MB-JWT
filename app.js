e0db2b95856480c285bf0e8c5d9df558c1795bbe
Rodrigo García Ávila <rodrigogarciaavila26@gmail.com>
2020-08-21 17:36:32 -0500

    API

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const app = express();
 const path = require('path');
-require('dotenv').config({ path: 'variables.env' });
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
@@ -18,30 +17,10 @@ app.use(express.static(path.join(__dirname, 'public'))); //path/public
 const users = require('./routes/userRoutes');
 app.use("/users", users);
 
-// Definir un domino para cibir peticiones
-const whiteList = [process.env.FRONTEND_URL];
-const corsOptions = {
-    origin: (origin, callback) => {
-        // Revisar si el domino está en el whitelist
-        console.log(origin);
-        const exist = whiteList.some(dominio => dominio === origin);
-        if (exist) {
-            callback(null, true);
-        } else {
-            callback(new Error('No permitido por CORS'));
-        }
-    }
-}
-
-app.use(cors(corsOptions));
 
 
-const host = process.env.HOST || '0.0.0.0';
-const port = process.env.PORT || 5000;
+app.use(cors());
 
-app.listen(port, host, () => {
-    console.log("Server works!!");
-});
 
 // app.set('puerto', process.env.PORT || 5000);
 // app.listen(app.get('puerto'), () => {
